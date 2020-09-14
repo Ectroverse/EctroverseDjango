15131356cc647a6189cb891557a566c8bec02105
vsavko <35723434+vsavko@users.noreply.github.com>
2020-09-14 17:51:41 +0300

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from django.conf.urls import url
 
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL,
+                              document_root=settings.MEDIA_ROOT)
 
 urlpatterns = [
     path('', views.headquarters, name='headquarters'), # root page
