5537a529d6129ea85cf15de174ae7b91bb7c85a2
vsavko <35723434+vsavko@users.noreply.github.com>
2021-03-09 16:44:10 +0300

    Update Main.java

--- java/Main.java
+++ java/Main.java
@@ -9,7 +9,7 @@ public class Main
 	long connectionTime = 0;
 	long statTime = 0;
 	long resultTime = 0;
-	long printTime = 0;
+	long executeBatchTime = 0;
          try {
 	 	//long startTime = System.nanoTime();
 		Connection con = DriverManager.getConnection("jdbc:postgresql://ectroversedjango_db_1:5432/djangodatabase", "dbadmin", "pass");
@@ -19,24 +19,29 @@ public class Main
 		ResultSet resultSet = statement.executeQuery("SELECT * FROM app_userstatus");
 		resultTime = System.nanoTime();
 		//System.out.println(resultSet);
+		 String compiledQuery = "INSERT INTO app_userstatus(id, fleet_readiness)" +
+                " VALUES" + "(?, ?)";
+        	preparedStatement = connection.prepareStatement(compiledQuery);
+		 
 		while(resultSet.next()){
-			String name = resultSet.getString("user_name");
-			String race = resultSet.getString("race");
-			//int age = resultSet.getInt("user");
-		System.out.println(name + " " + race + " ");
-
-		
+			int id = resultSet.getInt("id")
+			int fleet_readiness = resultSet.getInt("fleet_readiness")
+				
+			System.out.println(id);
+			preparedStatement.setInt(1, id);
+			preparedStatement.setInt(2, fleet_readiness+2);
+			preparedStatement.addBatch();
 		}
-		printTime = System.nanoTime();
+		executeBatchTime = System.nanoTime();
+		int[] inserted = preparedStatement.executeBatch();
+		System.out.println(Arrays.toString(inserted));
 	}
 	catch (Exception e) {
             System.out.println("exception " +  e.getMessage());
         }
+	     
 	long endTime = System.nanoTime();
-    	System.out.println("connection time " + (double)(connectionTime - startTime)/1_000_000_000.0);
- 	System.out.println("stat time " + (double)(endTime- statTime)/1_000_000_000.0);
-	System.out.println("result time " + (double)(endTime- resultTime)/1_000_000_000.0);
-	System.out.println("print time " + (double)(endTime- resultTime)/1_000_000_000.0);
+    	System.out.println("connection time " + (double)(endTime - executeBatchTime)/1_000_000_000.0);
 	System.out.println((double)(endTime-startTime)/1_000_000_000.0);
     }
 }
