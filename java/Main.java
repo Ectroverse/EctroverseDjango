5a975fbbf89dd93f7f694a14a1b32db90ddd58dd
vsavko <vsavko@gmail.com>
2021-03-10 10:17:46 +0300

    modified java files

--- java/Main.java
+++ java/Main.java
@@ -4,7 +4,7 @@ public class Main
 {
 
 
-     public static void main(String[] args) {
+    public static void main(String[] args) {
 	long startTime = System.nanoTime();
 	long connectionTime = 0;
 	long statTime = 0;
@@ -14,14 +14,16 @@ public class Main
 	 	//long startTime = System.nanoTime();
 		Connection con = DriverManager.getConnection("jdbc:postgresql://ectroversedjango_db_1:5432/djangodatabase", "dbadmin", "pass");
 		connectionTime = System.nanoTime();
+		
 		Statement statement = con.createStatement();
- 		statTime = System.nanoTime();
+ 		/*statTime = System.nanoTime();
 		ResultSet resultSet = statement.executeQuery("SELECT * FROM app_userstatus");
-		resultTime = System.nanoTime();
+		resultTime = System.nanoTime();*/
+		
 		//System.out.println(resultSet);
-		 String compiledQuery = "INSERT INTO app_userstatus(id, fleet_readiness)" +
+		 /*String compiledQuery = "INSERT INTO app_userstatus(id, fleet_readiness)" +
                 " VALUES" + "(?, ?)";
-        	preparedStatement = connection.prepareStatement(compiledQuery);
+        	PreparedStatement preparedStatement = connection.prepareStatement(compiledQuery);
 		 
 		while(resultSet.next()){
 			int id = resultSet.getInt("id")
@@ -34,14 +36,50 @@ public class Main
 		}
 		executeBatchTime = System.nanoTime();
 		int[] inserted = preparedStatement.executeBatch();
-		System.out.println(Arrays.toString(inserted));
+		System.out.println(Arrays.toString(inserted));*/
+		
+		 String compiledQuery2 = "UPDATE \"PLANET\" SET max_population = ? "+
+				 + " current_population = ?" +
+				 + " protection = ?" +
+		 		+ "WHERE id = ?" ;
+		 PreparedStatement preparedStatement = con.prepareStatement(compiledQuery2);
+		 
+		ResultSet planets = statement.executeQuery("SELECT * FROM \"PLANET\"");
+		long t0 = System.nanoTime();
+		while(planets.next()){
+			int id = planets.getInt("id");
+			int max_population = planets.getInt("max_population");
+			int size = planets.getInt("size");
+			int total = size * 4 ;
+			
+			
+			
+			preparedStatement.setInt(4, id);
+			preparedStatement.setInt(3, 2435345);
+			preparedStatement.setInt(2, 345);
+			preparedStatement.setInt(1, total+2);
+			preparedStatement.addBatch();
+					
+			//# Update Population
+	        /*        planet.max_population = (planet.size * population_size_factor)
+	                planet.max_population += (planet.cities * building_production_cities)
+	                planet.max_population *= (1.00 + 0.01 * status.research_percent_population)*/
+			
+		}
+		long t1 = System.nanoTime();
+		preparedStatement.executeBatch();
+		long t2= System.nanoTime();
+		System.out.println("planets execution 1: " + (double)(t1 - t0)/1_000_000_000.0);
+		System.out.println("planets execution 2: " + (double)(t2 - t1)/1_000_000_000.0);
 	}
 	catch (Exception e) {
             System.out.println("exception " +  e.getMessage());
         }
 	     
 	long endTime = System.nanoTime();
-    	System.out.println("connection time " + (double)(endTime - executeBatchTime)/1_000_000_000.0);
-	System.out.println((double)(endTime-startTime)/1_000_000_000.0);
+	
+    	System.out.println("connection time " + (double)(connectionTime - startTime)/1_000_000_000.0);
+    	
+    	System.out.println((double)(endTime-startTime)/1_000_000_000.0);
     }
 }
