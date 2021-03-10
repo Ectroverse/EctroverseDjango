978f018cf5684d403356370d9bc4e0126bf4f854
vsavko <35723434+vsavko@users.noreply.github.com>
2021-03-10 18:16:35 +0300

    Update Main.java
    add contants

--- java/Main.java
+++ java/Main.java
@@ -6,6 +6,122 @@ public class Main
 
 
     public static void main(String[] args) {
+	//some constants temporarily written here, fetch them from app/constants.py later
+	int population_size_factor  = 20;
+	int building_production_cities = 10000;
+	int energy_decay_factor = 0.005;
+	int crystal_decay_factor = 0.02;
+	int upkeep_solar_collectors = 0.0;
+	int upkeep_fission_reactors = 20.0;
+	int upkeep_mineral_plants = 2.0;
+	int upkeep_crystal_labs = 2.0;
+	int upkeep_refinement_stations = 2.0;
+	int upkeep_cities = 4.0;
+	int upkeep_research_centers = 1.0;
+	int upkeep_defense_sats = 4.0;
+	int upkeep_shield_networks = 16.0;
+	int networth_per_building = 8;
+	int building_production_solar = 12;
+	int building_production_fission = 40;
+	int building_production_mineral = 1;
+	int building_production_crystal = 1;
+	int building_production_ectrolium = 1;
+	int building_production_cities = 1000;
+	int building_production_research = 6;
+	float [] unit_upkeep = {2.0, 1.6, 3.2, 12.0, 18.0, 0.4, 0.6, 2.8, 0.0, 0.8, 0.8, 2.4, 60.0};
+	HashMap<String,HashMap<String, double>> race_info_list = new HashMap<>(); 
+	HashMap<String, double> harks = new HashMap<>();
+		harks.put("pop_growth", 0.8*0.02);
+		harks.put("research_bonus_military",    1.2);
+		harks.put("research_bonus_construction", 1.2);
+		harks.put("research_bonus_tech",         1.2);
+		harks.put("research_bonus_energy",       1.2);
+		harks.put("research_bonus_population",   1.2);
+		harks.put("research_bonus_culture",      0.6);
+		harks.put("research_bonus_operations",   1.2);
+		harks.put("research_bonus_portals",      1.2);
+		harks.put("energy_production",    0.9);
+		harks.put("mineral_production",   1.0);
+		harks.put("crystal_production",   1.25);
+		harks.put("ectrolium_production",   1.0);
+	race_info_list.put("Harks",harks);
+	HashMap<String, double> manticarias = new HashMap<>();
+		manticarias.put("pop_growth", 0.9*0.02);
+		manticarias.put("research_bonus_military",    0.9);
+		manticarias.put("research_bonus_construction", 0.9);
+		manticarias.put("research_bonus_tech",         0.9);
+		manticarias.put("research_bonus_energy",       0.9);
+		manticarias.put("research_bonus_population",   0.9);
+		manticarias.put("research_bonus_culture",      1.8);
+		manticarias.put("research_bonus_operations",   0.9);
+		manticarias.put("research_bonus_portals",      0.9);
+		manticarias.put("energy_production",    1.4);
+		manticarias.put("mineral_production",   1.0);
+		manticarias.put("crystal_production",   1.0);
+		manticarias.put("ectrolium_production",   1.0);
+	race_info_list.put("Manticarias",manticarias);  
+	HashMap<String, double> foohons = new HashMap<>();
+		foohons.put("pop_growth", 0.8*0.02);
+		foohons.put("research_bonus_military",    1.5);
+		foohons.put("research_bonus_construction", 1.5);
+		foohons.put("research_bonus_tech",         1.5);
+		foohons.put("research_bonus_energy",       1.5);
+		foohons.put("research_bonus_population",   1.5);
+		foohons.put("research_bonus_culture",      1.5);
+		foohons.put("research_bonus_operations",   1.5);
+		foohons.put("research_bonus_portals",      1.5);
+		foohons.put("energy_production",    0.8);
+		foohons.put("mineral_production",   1.0);
+		foohons.put("crystal_production",   1.0);
+		foohons.put("ectrolium_production",   1.2);
+	race_info_list.put("Foohons",foohons);  
+	HashMap<String, double> spacebournes = new HashMap<>();
+		spacebournes.put("pop_growth", 1.2*0.02);
+		spacebournes.put("research_bonus_military",    1.1);
+		spacebournes.put("research_bonus_construction", 1.1);
+		spacebournes.put("research_bonus_tech",         0.6);
+		spacebournes.put("research_bonus_energy",       1.1);
+		spacebournes.put("research_bonus_population",   1.1);
+		spacebournes.put("research_bonus_culture",      1.1);
+		spacebournes.put("research_bonus_operations",   1.1);
+		spacebournes.put("research_bonus_portals",      1.1);
+		spacebournes.put("energy_production",    1.3);
+		spacebournes.put("mineral_production",   1.0);
+		spacebournes.put("crystal_production",   1.0);
+		spacebournes.put("ectrolium_production",   1.0);
+	race_info_list.put("Spacebournes",spacebournes);  
+	HashMap<String, double> dreamweavers = new HashMap<>();
+		dreamweavers.put("pop_growth", 1.1*0.02);
+		dreamweavers.put("research_bonus_military",    1.4);
+		dreamweavers.put("research_bonus_construction", 1.4);
+		dreamweavers.put("research_bonus_tech",         2.8);
+		dreamweavers.put("research_bonus_energy",       1.4);
+		dreamweavers.put("research_bonus_population",   1.4);
+		dreamweavers.put("research_bonus_culture",      1.4);
+		dreamweavers.put("research_bonus_operations",   1.4);
+		dreamweavers.put("research_bonus_portals",      1.4);
+		dreamweavers.put("energy_production",    0.8);
+		dreamweavers.put("mineral_production",   1.0);
+		dreamweavers.put("crystal_production",   1.0);
+		dreamweavers.put("ectrolium_production",   1.0);
+	race_info_list.put("Dreamweavers",dreamweavers);  
+	HashMap<String, double> wookiees = new HashMap<>();
+		wookiees.put("pop_growth", 1.2*0.02);
+		wookiees.put("research_bonus_military",    1.0);
+		wookiees.put("research_bonus_construction", 2.0);
+		wookiees.put("research_bonus_tech",         1.0);
+		wookiees.put("research_bonus_energy",       1.0);
+		wookiees.put("research_bonus_population",   2.0);
+		wookiees.put("research_bonus_culture",      1.0);
+		wookiees.put("research_bonus_operations",   1.0);
+		wookiees.put("research_bonus_portals",      2.0);
+		wookiees.put("energy_production",    0.7);
+		wookiees.put("mineral_production",   1.25);
+		wookiees.put("crystal_production",   1.25);
+		wookiees.put("ectrolium_production",   1.0);
+	race_info_list.put("wookiees",wookiees);  
+
+		
 	long startTime = System.nanoTime();
 	long connectionTime = 0;
 	long statTime = 0;
@@ -151,26 +267,94 @@ public class Main
 			HashMap<String,Long> rowLong  = usersLong.get(i);
 				
 			int userID = rowInt.get("user_id");
-			ResultSet planets = statement.executeQuery("SELECT * FROM \"PLANET\" WHERE id = " + userID);
+			
+			int fr = Math.min(rowInt.get(fleet_readiness)+2, rowInt.get(fleet_readiness_max));
+			userStatusUpdateStatement.setInt(1, fr); 
+			int pr = Math.min(rowInt.get(psychic_readiness)+2, rowInt.get(psychic_readiness_max));
+			userStatusUpdateStatement.setInt(2, pr); 
+			int ar = Math.min(rowInt.get(agent_readiness)+2, rowInt.get(agent_readiness_max));
+			userStatusUpdateStatement.setInt(2, ar); 
+			 
+			 /*for job in Construction.objects.filter(user=status.user.id):
+                job.ticks_remaining -= 1
+                if job.ticks_remaining <= 0:
+                    if job.building_type == 'SC':
+                        job.planet.solar_collectors += job.n
+                    if job.building_type == 'FR':
+                        job.planet.fission_reactors += job.n
+                    if job.building_type == 'MP':
+                        job.planet.mineral_plants += job.n
+                    if job.building_type == 'CL':
+                        job.planet.crystal_labs += job.n
+                    if job.building_type == 'RS':
+                        job.planet.refinement_stations += job.n
+                    if job.building_type == 'CT':
+                        job.planet.cities += job.n
+                    if job.building_type == 'RC':
+                        job.planet.research_centers += job.n
+                    if job.building_type == 'DS':
+                        job.planet.defense_sats += job.n
+                    if job.building_type == 'SN':
+                        job.planet.shield_networks += job.n
+                    if job.building_type == 'PL':
+                        job.planet.portal = True
+                        job.planet.portal_under_construction = False
+                    job.planet.buildings_under_construction -= job.n
+                    job.planet.save()
+
+                    # TODO ADD IT TO THE NEWS!
+
+                    job.delete()
+                else:
+                    job.save()*/
+			 
+		/*
+		   # Repeat for units
+            main_fleet = Fleet.objects.get(owner=status.user.id, main_fleet=True) # should only ever be 1
+            for job in UnitConstruction.objects.filter(user=status.user.id):
+                job.ticks_remaining -= 1
+                if job.ticks_remaining <= 0:
+                    setattr(main_fleet, job.unit_type, getattr(main_fleet, job.unit_type) + job.n)
+                    main_fleet.save()
+                    # TODO ADD IT TO THE NEWS!
+                    job.delete()
+                else:
+                    job.save()*/
+			 
+			 
+			 
+			ResultSet resultSet = statement.executeQuery("SELECT * FROM \"PLANET\" WHERE id = " + userID);
+			 
+			long population = 0;
+			int num_planets = 0;
+			long cmdTickProduction_solar = 0;
+			long cmdTickProduction_fission = 0;
+			int cmdTickProduction_mineral = 0;
+			int cmdTickProduction_crystal = 0;
+			int cmdTickProduction_ectrolium = 0;
+			long cmdTickProduction_cities = 0;
+			int cmdTickProduction_research = 0;
+
+			int total_solar_collectors = 0;
+			int total_fission_reactors = 0;
+			int total_mineral_plants = 0;
+			int total_crystal_labs = 0;
+			int total_refinement_stations = 0;
+			int total_cities = 0;
+			int total_research_centers = 0;
+			int total_defense_sats = 0;
+			int total_shield_networks = 0;
+			int total_portals = 0;
 			
 			
 			//loop over planets of each user
 			while(planets.next()){
-				/*int id = planets.getInt("id");
-				int max_population = planets.getInt("max_population");
-				int size = planets.getInt("size");
-				int total = size * 4 ;
-
-				preparedStatement.setInt(4, id);
-				preparedStatement.setInt(3, 2435345);
-				preparedStatement.setInt(2, 345);
-				preparedStatement.setInt(1, total+2);
-				preparedStatement.addBatch();*/
-
-				//# Update Population
-			        /*planet.max_population = (planet.size * population_size_factor)
-				planet.max_population += (planet.cities * building_production_cities)
-				planet.max_population *= (1.00 + 0.01 * status.research_percent_population)*/
+				num_planets++;
+				int max_population = (resultSet.getInt(size) * population_size_factor)
+				max_population += (resultSet.getInt(cities) * building_production_cities)
+				max_population *= (1.00 + 0.01 * status.research_percent_population)
+				//Update Population
+				
 		
 			}
 		}
