cc2e09de00080409bf631a65edf6213a10cbbe34
vsavko <35723434+vsavko@users.noreply.github.com>
2021-04-10 20:54:22 +0300

    Update Settings.java

--- java/Settings.java
+++ java/Settings.java
@@ -1,8 +1,8 @@
 package org.ectroverse.processtick;
 
 public final class Settings {	
-	public static final String connectionPath = "jdbc:postgresql://ectroversedjango_db_1:5432/djangodatabase"; //specify your db connection, e.g. jdbc:postgresql://ectroversedjango_db_1:5432/djangodatabase
-	public static final String dbUserName = "dbadmin"; //your database username
-	public static final String dbPass = "admin12345"; //your database password
+	public static final String connectionPath = ""; //specify your db connection, e.g. jdbc:postgresql://ectroversedjango_db_1:5432/djangodatabase
+	public static final String dbUserName = ""; //your database username
+	public static final String dbPass = ""; //your database password
 	public static final int tickTime = 10; //specify in tick time in seconds
 }
